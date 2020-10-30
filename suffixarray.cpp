ec3e24f72db2335bfba236a108d7e270b25eac5c
Jan Adelmann <jan.adelmann@gmail.com>
2020-10-30 14:55:32 +0100

    work work

--- suffixarray.cpp
+++ suffixarray.cpp
@@ -8,14 +8,34 @@
 struct suffix
 {
   int index;
-  char *suffix;
+  char *suffix_i;
 };
+
+// helper function to compare two suffixes, used by sort function
+int cmp(struct suffix a, struct suffix b)
+{
+    return std::strcmp(a.suffix_i, b.suffix_i) < 0? 1 : 0;
+}
 
 // This function builds the suffix array. It returns the array with additional
 // index
-int constructSuffixArray(char *str, int len)
+int *constructSuffixArray(char *str, int len)
 {
-  
+  // struct to store the suffixex and the indexes of length len
+  struct suffix suffixes[len];
+
+  // store all suffixes in an array such that we can retain its index when sorting
+  // the suffixes
+
+  for (int i = 0; i < len; ++i)
+  {
+    suffixes[i].index = i;
+    suffixes[i].suffix_i = (len+i);
+  }
+
+  std::sort(suffixes.begin(), suffixes.end(), cmp);
+
+
 }
 
 
