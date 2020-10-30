f946f65643be6c84bb2e5f219d29a8ee629bf848
Jan Adelmann <jan.adelmann@gmail.com>
2020-10-30 14:42:10 +0100

    added suffix stuct and main function to construct the suffix array

--- suffixarray.cpp
+++ suffixarray.cpp
@@ -1,7 +1,27 @@
 // Program that creates a suffix array from a string in a sequential manner
 
-#include <iostream> 
-#include <cstring> 
-#include <algorithm> 
-#include <time>
+#include <iostream>
+#include <cstring>
+#include <algorithm>
 
+// The information of one suffix is stored in this structure
+struct suffix
+{
+  int index;
+  char *suffix;
+};
+
+// This function builds the suffix array. It returns the array with additional
+// index
+int constructSuffixArray(char *str, int len)
+{
+  
+}
+
+
+int main()
+{
+  char str[] = "hello";
+  int len = std::strlen(str);
+  std::cout << "process finished" << std::endl;
+}
