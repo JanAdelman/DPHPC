740463f6265c7da33fd86654bdb4cb51c7b97af5
rayastoyanova <70844702+rayastoyanova@users.noreply.github.com>
2020-11-05 12:50:25 +0100

    Added rebucketing feature

---
+++ src/rebucket.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <tuple>
+
+typedef std::vector<std::tuple<std::string,int>> tuple_vector;
+
+tuple_vector rebucket(tuple_vector vec){
+    tuple_vector B;
+    std::string prev_string=std::get<0>(vec[0]);
+    int prev_index=0;
+    int count=0;
+    for(tuple_vector::const_iterator i=vec.begin(); i!=vec.end();i++){
+        if(std::get<0>(*i)==prev_string){
+            B[count]=std::make_tuple(std::get<0>(*i),prev_index);
+        }
+        else{
+            B[count]=std::make_tuple(std::get<0>(*i),count);
+            prev_string=std::get<0>(*i);
+            prev_index= count;
+        }
+        count++;
+    }
+    return B;
+}
