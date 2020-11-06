a0717fcd4ef7e0be9ed1a572f618011f33e1095f
Gian Hiltbrunner <gian.silvan.hiltbrunner@gmail.com>
2020-11-06 14:58:36 +0100

    Reformating

---
+++ src/helper.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <tuple>
+
+/*
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch_amalgamated.hpp"
+*/
+
+typedef std::vector<std::tuple<std::string, int>> tuple_vector;
+typedef std::vector<std::tuple<int, int, int>> triple_vector;
+
+tuple_vector get_kmers(const std::string &input, const int k)
+{
+    tuple_vector kmers;
+
+    for (std::string::const_iterator i = input.begin(); i != input.end(); i++)
+    {
+        kmers.push_back(
+            std::make_tuple(
+                std::string(i, i + k),
+                int((i - input.begin()))));
+    }
+    return kmers;
+}
+
+void sort_tuple_vector(tuple_vector &input)
+{
+    std::sort(input.begin(), input.end(), [&](auto a, auto b) {
+        return std::lexicographical_compare(std::get<0>(a).begin(), std::get<0>(a).end(),
+                                            std::get<0>(b).begin(), std::get<0>(b).end());
+    });
+}
+
+void print_tuple_vector(const tuple_vector &input)
+{
+
+    for (tuple_vector::const_iterator i = input.begin(); i != input.end(); ++i)
+    {
+        std::cout << std::get<0>(*i) << "," << std::get<1>(*i) << "|";
+    }
+    std::cout << std::endl;
+}
+
+void print_triple_vector(const triple_vector &input)
+{
+
+    for (triple_vector::const_iterator i = input.begin(); i != input.end(); ++i)
+    {
+        std::cout << std::get<0>(*i) << "," << std::get<1>(*i) << "," << std::get<2>(*i) << "|";
+    }
+    std::cout << std::endl;
+}
+
+void sort_triple_vector(triple_vector &input)
+{
+    
+    std::sort(input.begin(), input.end(), [&](auto a, auto b) {
+        return (std::get<0>(a) < std::get<0>(b)) ? true : (
+            std::get<1>(a) < std::get<1>(b) ? true : (
+                    std::get<2>(a) < std::get<2>(b)
+                )
+        );
+    });
+    
+}
+
+tuple_vector rebucket(tuple_vector vec)
+{
+    tuple_vector B;
+    std::string prev_string = std::get<0>(vec[0]);
+    int prev_index = 0;
+    int count = 0;
+    for (tuple_vector::const_iterator i = vec.begin(); i != vec.end(); i++)
+    {
+        if (std::get<0>(*i) == prev_string)
+        {
+            B.push_back(std::make_tuple(std::get<0>(*i), prev_index));
+        }
+        else
+        {
+            B.push_back(std::make_tuple(std::get<0>(*i), count));
+            prev_string = std::get<0>(*i);
+            prev_index = count;
+        }
+        count++;
+    }
+    return B;
+}
+/*
+int main (){
+    std::string test = "bananaahahahhahahh";
+    print_tuple_vector(get_kmers(test, 2));
+} 
+
+std::tuple<std::string, int> data[] = {std::make_tuple("ba",0),std::make_tuple("an",1),std::make_tuple("na",1)};
+tuple_vector test (data, data + sizeof(data) / sizeof(int) );
+
+TEST_CASE( "Kmers are computed!", "[kmer]" ) {
+    REQUIRE( get_kmers("bana", 2) == test );
+}
+
+*/
--- src/main.cpp
+++ src/main.cpp
@@ -5,39 +5,41 @@
 #include <vector>
 #include <tuple>
 
-#include "prefix_doubling.cpp"
-#include "rebucket.cpp"
+#include "helper.cpp"
 
 typedef std::vector<std::tuple<std::string, int>> tuple_vector;
 typedef std::vector<std::tuple<int, int, int>> triple_vector;
 
 int main (){
-    std::string test = "bananaahahahhahahh";
-    int k=2;
+    std::string test = "mississippi$";
+    int k=1;
     tuple_vector test_vec =  get_kmers(test, k);
-    print_tuple_vector(test_vec);
+    //print_tuple_vector(test_vec);
     sort_tuple_vector(test_vec);
-    print_tuple_vector(test_vec);
+    //print_tuple_vector(test_vec);
     tuple_vector B = rebucket(test_vec);
-    print_tuple_vector(B);   
+    //print_tuple_vector(B);   
     for(int h=k; h<=test.size();h*=2){
         tuple_vector B_new(B.size());
         for(int i=0;i<B.size();i++){
             B_new[std::get<1>(test_vec[i])]=B[i];
         }
         B=B_new;
-        print_tuple_vector(B);
+        //print_tuple_vector(B);
         triple_vector B2(B.size());
         for(int i=0;i<B.size();i++){
             if(i<B.size()-h){
                 B2[i]= std::make_tuple(std::get<1>(B[i]),std::get<1>(B[i+h]),i);
             }
             else{
-                 B2[i]=std::make_tuple(std::get<1>(B[i]),0,i);
+                B2[i]=std::make_tuple(std::get<1>(B[i]),0,i);
             }
         }
+        print_triple_vector(B2);
+        sort_triple_vector(B2);
+        std::cout << "TRIPLE" << std::endl;
+        print_triple_vector(B2);
     }
-
 
     return 0;
 } 
