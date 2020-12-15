af33175c5c161c5dead35894100e2c4b7a67a375
Jan Adelmann <jan.adelmann@gmail.com>
2020-12-15 20:03:19 +0100

    1. Plot finished

--- src/plotting.py
+++ src/plotting.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 df = pd.read_csv('performance.csv', delimiter = ',')
 
@@ -8,25 +9,10 @@ unique = df.nr_cores.unique()
 mean_time = df.groupby(['nr_cores', 'str_length']).mean()
 std_v = df.groupby(['nr_cores', 'str_length']).std()
 
-
-for i in unique:
-    values = df['nr_cores'] == i
-    subset = df[values]
-    plt.plot(subset['str_length'], subset['time'], 'go--', color = 'blue', linewidth=2, markersize=12)
-    plt.title(('Benchmarking with {} Cores').format(i))
-    plt.xlabel('Stringe size')
-    plt.ylabel('time [s]')
-
-    #plt.show()                                                        #print(df[values])
-
-#print(list(mean_time))
-#print(mean_time)
-
 values = mean_time['nr_cores'] == 4
-#print(values)
 
-#for i in unique:
-#    values = mean_time['nr_cores'] == i
-#    subset = mean_time[values]
+mean_time =mean_time.drop('Unnamed: 0', axis = 1).reset_index()
 
-#print()
+sns.pointplot(data=mean_time, x="str_length", y="time", hue="nr_cores", legend='Nr. Cores')
+df_raw = df.drop('Unnamed: 0', axis =1)
+sns.pointplot(data=mean_time, x="str_length", y="time", hue="nr_cores", err_style="bars", ci=90)
