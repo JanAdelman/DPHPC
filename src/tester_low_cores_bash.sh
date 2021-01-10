792093955d5d5214dac874a7af3774dd31913baf
Jan Adelmann <jan.adelmann@gmail.com>
2021-01-10 21:56:14 +0100

    Bash script to run all inputs on smaller amount of cores

---
+++ src/tester_low_cores_bash.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mpicxx -std=c++11 -O3 main_mpi.cpp -o main
+
+echo "" > performance.csv
+echo "String_lenght, #Cores, Repetition, Overall, Min, Max, Average" > performance.csv 
+
+declare -a cores=(16 32 64 128)
+
+for file in ./data/*
+do
+for i in ${cores[@]}
+do
+for numbers in {1..10}
+do
+arr_settings+=($file $i $numbers)
+echo -e "${arr_settings[0]},${arr_settings[1]}, ${arr_settings[2]},  \c" >> performance.csv
+mpiexec -np $i ./main $file >> performance.csv 
+declare -a arr_settings=()
+done
+done
+done
