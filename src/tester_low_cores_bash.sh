22b1a8278ba9092608f2b192d5e01798ed3358d9
Jan Adelmann <janade@eu-login-13.euler.ethz.ch>
2021-01-11 14:27:12 +0100

    working bash script for low core numbers

--- src/tester_low_cores_bash.sh
+++ src/tester_low_cores_bash.sh
@@ -5,7 +5,7 @@ mpicxx -std=c++11 -O3 main_mpi.cpp -o main
 echo "" > performance.csv
 echo "String_lenght, #Cores, Repetition, Overall, Min, Max, Average" > performance.csv 
 
-declare -a cores=(16 32 64 128)
+declare -a cores=(32 64 128)
 
 for file in ./data/*
 do
@@ -14,8 +14,8 @@ do
 for numbers in {1..10}
 do
 arr_settings+=($file $i $numbers)
-echo -e "${arr_settings[0]},${arr_settings[1]}, ${arr_settings[2]},  \c" >> performance.csv
-mpiexec -np $i ./main $file >> performance.csv 
+echo -e "${arr_settings[0]},${arr_settings[1]}, ${arr_settings[2]},  \c" >> performance.csv 
+mpirun -n ${i} ./main ${file} >> performance.csv 
 declare -a arr_settings=()
 done
 done
